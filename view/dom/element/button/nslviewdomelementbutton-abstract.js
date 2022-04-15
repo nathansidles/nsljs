a59705293bfdda5deace99458a25383b0ea72c03
Nathan Sidles <nsidles@gmail.com>
2022-04-15 06:15:30 -0700

    updated code

--- view/dom/element/button/nslviewdomelementbutton-abstract.js
+++ view/dom/element/button/nslviewdomelementbutton-abstract.js
@@ -11,7 +11,7 @@ export default class NSLViewDOMElementButtonAbstract extends NSLViewDOMElementAb
       let env = this;
       this['$node'] = this.create({
         'appendee' : object.appendee, 'tagName' : 'div', 'classes' : [ 'nsl-element-button' ] });
-      this.content = new NSLViewDOMElement({ 'appendee' : this['$node'], 'tagName' : 'div', 'classes' : [ 'nsl-title' ], 'content' : object.content });
+      // this.content = new NSLViewDOMElement({ 'appendee' : this['$node'], 'tagName' : 'div', 'classes' : [ 'nsl-title' ], 'content' : object.content });
     }
   }
 
